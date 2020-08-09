29a4cccfbfe993c795aaa6979aaed1ddc438f1ee
Patryqss <gpatryk14@gmail.com>
2020-08-09 20:41:17 +0200

    InfoWindow fixed

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const colors = ['https://i.imgur.com/NsQQAix.png', 'https://i.imgur.com/5cB7OUv.png', 'https://i.imgur.com/iBtUyCa.png', 'https://i.imgur.com/FwyXica.png', 'https://i.imgur.com/3bXECRM.png', 'https://i.imgur.com/6IF42VT.png', 'https://i.imgur.com/2qYxhkf.png', 'https://i.imgur.com/9JRtUiL.png', 'https://i.imgur.com/6e50x9Y.png', 'https://i.imgur.com/zTW0MaD.png', 'https://i.imgur.com/2xiKuH6.png', 'https://i.imgur.com/nltkFOq.png', 'https://i.imgur.com/cUGYRvC.png', 'https://i.imgur.com/ipbn3SR.png', 'https://i.imgur.com/KVUb4l7.png', 'https://i.imgur.com/ABY52gh.png', 'https://i.imgur.com/auR5Z0M.png', 'https://i.imgur.com/8q3VqKE.png', 'https://i.imgur.com/gLTEbzv.png', 'https://i.imgur.com/Ll1k7wz.png'] //markers colors for categories
-
+//marker colors for categories
+const colors = [
+  'https://i.imgur.com/NsQQAix.png', 'https://i.imgur.com/5cB7OUv.png', 'https://i.imgur.com/iBtUyCa.png',  'https://i.imgur.com/FwyXica.png', 'https://i.imgur.com/3bXECRM.png', 'https://i.imgur.com/6IF42VT.png', 'https://i.imgur.com/2qYxhkf.png', 'https://i.imgur.com/9JRtUiL.png', 'https://i.imgur.com/6e50x9Y.png', 'https://i.imgur.com/zTW0MaD.png', 'https://i.imgur.com/2xiKuH6.png', 'https://i.imgur.com/nltkFOq.png', 'https://i.imgur.com/cUGYRvC.png', 'https://i.imgur.com/ipbn3SR.png', 'https://i.imgur.com/KVUb4l7.png', 'https://i.imgur.com/ABY52gh.png', 'https://i.imgur.com/auR5Z0M.png', 'https://i.imgur.com/8q3VqKE.png', 'https://i.imgur.com/gLTEbzv.png', 'https://i.imgur.com/Ll1k7wz.png',
+];
 
 class GoogleMap extends Component {
   constructor(props) {
@@ -12,16 +14,16 @@ class GoogleMap extends Component {
     this.state = {
       coordinates: [],
       addressError: '',
-      showMarker: false,
-      markerCat: '',
+      activeMarker: {},
+      selectedPlace: {},
+      showingInfoWindow: false,
       markerAddress: '',
-      marker: ''
+      markerCat: '',
     };
   }
 
   componentDidUpdate(prevProps) {
-    if(this.props.addresses !== prevProps.addresses)
-      this.getLatLong();
+    if (this.props.addresses !== prevProps.addresses) this.getLatLong();
   }
 
   getLatLong = () => {
@@ -29,7 +31,7 @@ class GoogleMap extends Component {
     const coordinates = [];
     const geocoder = new this.props.google.maps.Geocoder();
     for (let i = 0; i < addresses.length; i++) {
-      let address = `${addresses[i].state}, ${addresses[i].city}, ${addresses[i].zip}, ${addresses[i].address}`;
+      let address = `${addresses[i].state ? addresses[i].state + ' ' : ''}${addresses[i].city ? addresses[i].city + ' ' : ''}${addresses[i].zip ? addresses[i].zip + ' ' : ''}${addresses[i].address ? addresses[i].address : ''}`;
       geocoder.geocode({ address: address }, (results, status) => {
         if (status === 'OK') {
           let locationString = `${results[0].geometry.location}`;
@@ -41,7 +43,7 @@ class GoogleMap extends Component {
             this.setState({ coordinates: coordinates });
           }
         } else {
-            this.setState({ addressError: 'One or more of your addresses were invalid.' });
+          this.setState({ addressError: 'One or more of your addresses were invalid.' });
         }
       });
     }
@@ -50,18 +52,19 @@ class GoogleMap extends Component {
   displayMarkers = () => {
     let locationsWithCategories = this.state.coordinates.groupDynamically('category');
     return Object.keys(locationsWithCategories).map((key, index) => {
-        return locationsWithCategories[key].map(({ category, address, ...obj}) => {
-            return (
-                <Marker
-                  key={`${obj.lat},${obj.lng}`}
-                  id={`${obj.lat},${obj.lng}`}
-                  position={obj}
-                  icon={colors[index]}
-                  onClick={e => this.setState({ showMarker: true, markerCat: category, markerAddress: address, marker: e })}
-                />
-              );
-        })
-    })
+      return locationsWithCategories[key].map(({ category, address, ...obj }) => {
+        return (
+          <Marker
+            key={address}
+            id={address}
+            className={category}
+            position={obj}
+            icon={colors[index]}
+            onClick={this.onMarkerClick}
+          />
+        );
+      });
+    });
   };
 
   setMapBounds = () => {
@@ -72,6 +75,29 @@ class GoogleMap extends Component {
     return bounds;
   };
 
+  onMarkerClick = (props, marker) =>
+    this.setState({
+      activeMarker: marker,
+      selectedPlace: marker.position,
+      showingInfoWindow: true,
+      markerAddress: marker.id,
+      markerCat: marker.className,
+    });
+
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false,
+    });
+
+  onMapClicked = () => {
+    if (this.state.showingInfoWindow)
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false,
+      });
+  };
+
   render() {
     return (
       <>
@@ -79,20 +105,22 @@ class GoogleMap extends Component {
         <Map
           google={this.props.google}
           zoom={8}
+          onClick={this.onMapClicked}
           initialCenter={{ lat: 51.107883, lng: 17.038538 }}
           bounds={this.setMapBounds()}
         >
           {this.state.coordinates.length > 0 && this.displayMarkers()}
-        </Map>
-        {this.state.showMarker &&
-        <InfoWindow
-          marker={this.state.marker}>
+          <InfoWindow
+            marker={this.state.activeMarker}
+            onClose={this.onInfoWindowClose}
+            visible={this.state.showingInfoWindow}
+          >
             <div>
               <h1>{this.state.markerCat}</h1>
-              <h1>{this.state.markerAddress}</h1>
+              <h3>{this.state.markerAddress}</h3>
             </div>
-        </InfoWindow>}
-        {console.log(this.state)}
+          </InfoWindow>
+        </Map>
       </>
     );
   }
@@ -109,11 +137,11 @@ export default connect(mapStateToProps)(
 );
 
 // eslint-disable-next-line no-extend-native
-Array.prototype.groupDynamically = function(prop) {
-    return this.reduce(function(groups, item) {
-      var val = item[prop];
-      groups[val] = groups[val] || [];
-      groups[val].push(item);
-      return groups;
-    }, {});
-  }
+Array.prototype.groupDynamically = function (prop) {
+  return this.reduce(function (groups, item) {
+    var val = item[prop];
+    groups[val] = groups[val] || [];
+    groups[val].push(item);
+    return groups;
+  }, {});
+};
